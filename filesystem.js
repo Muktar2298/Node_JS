8ff4ea6c23d7197f09ceacac0e0a5557f5e71288
Md.Muktar Ali <muktar15-2298@diu.edu.bd>
2021-09-23 00:06:22 +0600

    file System Module

---
+++ filesystem.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+// --Asynchronous fs methodes --//
+//1.readFile()
+fs.readFile("myfile.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
+
+/* // --synchronous fs methods --//
+// 1.writeFileSync()
+fs.writeFileSync("myfile.txt", "Hello Programmers!");
+// 2.appendFileSync()
+fs.appendFileSync("myfile.txt", "Welcome to Node.JS Arena.");
+//3.readFileSync()
+fs.writeFileSync("myfile2.txt", "Exploring Node.JS");
+const data = fs.readFileSync("myfile2.txt");
+console.log(data.toString()); */
