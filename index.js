e64b6f4d52182a46b8e2baf71a474b7ca9a453a5
Md.Muktar Ali <muktar15-2298@diu.edu.bd>
2021-09-22 12:10:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let peoples = ["nishat", "hasan", "muktar"];
+
+const test = () => {
+  console.log("Testing the solting of JS Module");
+};
+let a = 20;
+let sum = a + 20;
+
+// console.log(module);
+module.exports = {
+  peoples,
+  test,
+  sum,
+};
